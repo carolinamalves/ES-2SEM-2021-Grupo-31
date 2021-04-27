647ce68863f2061a1106407918899a16f5720e2e
adilh <ahhad@iscte-iul.pt>
2021-04-27 15:46:14 +0100

    Alteração nome_class

--- grupo31/src/main/java/Grupo31/g31/Nom_class.java
+++ grupo31/src/main/java/Grupo31/g31/Nom_class.java
@@ -6,9 +6,9 @@ import java.lang.reflect.Method;
 public class Nom_class {
 	
 	private static int numberM;
-	
+	public String name;
 	public String getClassName() {
-		String className = this.getClass().getSimpleName(); 
+		String className = name; 
 		numberM=0;
 		return className;
 	}
