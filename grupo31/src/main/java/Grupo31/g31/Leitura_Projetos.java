241cd2992ce65df99851f2993090365cdc05c18d
adilh <ahhad@iscte-iul.pt>
2021-05-01 16:09:17 +0100

    Retira o final do path

--- grupo31/src/main/java/Grupo31/g31/Leitura_Projetos.java
+++ grupo31/src/main/java/Grupo31/g31/Leitura_Projetos.java
@@ -1,6 +1,8 @@
 package Grupo31.g31;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,6 +10,8 @@ import java.util.List;
 public class Leitura_Projetos {
 	  static ArrayList <File> ficheiro = new ArrayList<>();
 	 static String dirName;
+	  static ArrayList <String> fimdojava = new ArrayList<>();
+	  static Create_Method cm = new Create_Method();
 	  public static ArrayList<File> getFicheiro() {
 		  return ficheiro;
 	  }
@@ -22,12 +26,17 @@ public class Leitura_Projetos {
 	  public List <File> lista(String local_diretorio) {
 	      dirName = local_diretorio;
 		  File diretorio = new File(local_diretorio);
-
+		  List <File> file= new ArrayList<>();
 	        File[] listaFicheiros = diretorio.listFiles();
 	        ficheiro.addAll(Arrays.asList(listaFicheiros));
 	        for (File i : listaFicheiros) {
 	        	if (i.isFile() && i.getAbsolutePath().endsWith(".java")) {
-	                System.out.println(i.getAbsolutePath());
+	        //		System.out.println("caminho"+i.getAbsolutePath());
+	        	  
+	        		Path fim = Paths.get(i.getAbsolutePath());
+	        	    System.out.println(fim.getFileName().toString());
+	        	    fimdojava.add(fim.getFileName().toString());
+	        	     			        		
 	            } else if (i.isDirectory()) {
 	                ficheiro.addAll(lista(i.getAbsolutePath()));
 	            }
@@ -35,6 +44,21 @@ public class Leitura_Projetos {
 	        //System.out.println(fList);
 	       //System.out.println(getFicheiro());
 	        return ficheiro;
-	      
-	    }   
+	    }
+
+	public static ArrayList<String> getFimdojava() {
+		return fimdojava;
 	}
+  	  
+	 public static void main(String[] args) {
+ 		// grupo31\\
+	     Leitura_Projetos p = new Leitura_Projetos();		 
+ 		File f = new File("C:\\Users\\adilh\\git\\ES-2SEM-2021-Grupo-31\\");
+ 		 List<File> files=new ArrayList<>();
+ 		
+ 		 p.lista("C:\\Users\\adilh\\git\\ES-2SEM-2021-Grupo-31\\grupo31\\");
+ 		 System.out.println("dentro do array esta"+p.getFimdojava());
+	 }	 
+
+
+}
